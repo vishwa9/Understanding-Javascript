f2a27aa6d1e6101f32f086ae13671bcf3bc99fd1
vishwa9 <vishwajeet11104736@gmail.com>
2017-12-14 21:14:25 +0530

    topic closure added

---
+++ Understanding-Closures/app.js
@@ -0,0 +1,67 @@
+function greet(input){
+  return function(name){
+    console.log(input+' '+name);
+  }
+}
+
+var sayHi = greet('Hi!!');
+sayHi('vishwa');
+
+// little more complex example :)
+
+function buildFunction(){
+  var arr = [];
+
+  for(var i = 0; i < 3; i++){
+
+    arr.push(function(){
+      console.log(i);
+    });
+  }
+  return arr;
+}
+
+var run = buildFunction();
+run[0](); // 3
+run[1](); // 3
+run[2](); // 3
+
+// little change :P
+
+function buildFunction1(){
+  var arr = [];
+
+  for(let i = 0; i < 3; i++){  // ES2016 feature let assign each i at different
+                              //memory location
+
+    arr.push(function(){
+      console.log(i);
+    });
+  }
+  return arr;
+}
+
+var run = buildFunction1();
+run[0](); // 0
+run[1](); // 1
+run[2](); // 2
+
+// little more change :P
+
+function buildFunction2(){
+  var arr = [];
+
+  for(var i = 0; i < 3; i++){
+
+    arr.push(
+      (function(item){  //ES2015 feature IIFE
+      console.log(item)
+    })(i));
+  }
+  return arr;
+}
+
+var run1 = buildFunction2();
+run1[0]; // 0
+run1[1]; // 1
+run1[2]; // 2
